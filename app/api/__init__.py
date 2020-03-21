cf33d5da7c5abefc4546ae1bc9aa68d52d93daa5
The Polymath <das.sanctity.ds@gmail.com>
2020-03-21 18:41:50 +0100

    Added core app routes login, home ..

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -25,10 +25,30 @@ authorizations = {
     }
 }
 
+# The token decorator to protect my routes
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'API-KEY' in request.headers:
+            token = request.headers['API-KEY']
+            try:
+                data = jwt.decode(token, app.config.get('SECRET_KEY'))
+            except:
+                return {'message': 'Token is invalid.'}, 403
+        if not token:
+            return {'message': 'Token is missing or not found.'}, 401
+        if data:
+            pass
+        return f(*args, **kwargs)
+    return decorated
+
 api = Blueprint('api', __name__, template_folder = '../templates')
-apisec = Api( app=api, doc='/docs', version='1.0', title='News API.', \
+apisec = Api( app=api, doc='/docs', version='1.4', title='News API.', \
 description='', authorizations=authorizations)
 CORS(api, resources={r"/api/*": {"origins": "*"}})
+
+from . import schema
 
 uploader = apisec.parser()
 uploader.add_argument('file', location='files', type=FileStorage, required=True, help="You must parse a file")
@@ -42,10 +62,54 @@ apisec.add_namespace(search)
 
 login = apisec.namespace('/api/login', \
     description='This contains routes for core app data access. Authorization is required for each of the calls. \
-        To get this authorization, please contact out I.T Team ', \
+    To get this authorization, please contact out I.T Team ', \
     path='/v1/')
 
-post = apisec.namespace('/api/post', \
+home = apisec.namespace('/api/home/[user]', \
     description= "All routes under this section of the documentation are the open routes bots can perform CRUD action \
     on the application.", \
-    path = '/private/')
+    path = '/v1/')
+
+@login.doc(
+    security='KEY',
+    params={ 'id': 'ID of the post' },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@login.route('/login')
+class Login(Resource):
+    @token_required
+    @login.expect(schema.logindata)
+    def post(self):
+        return {}, 200
+
+@home.doc(
+    security='KEY',
+    params={ 'id': 'ID of the post' },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@home.route('/home/<user>')
+class Home(Resource):
+    @token_required
+    @home.marshal_with(schema.homedata)
+    def get(self):
+        return {}, 200        
--- app/api/v1/search.py
+++ app/api/v1/search.py
@@ -1,10 +1,17 @@
+########################################################
+# This file contains all search routes and sub routes  #
+# You can can add more routes to this file  if need be #
+########################################################
+# Make sure you don't mess it up.
+
 from flask_restplus import Namespace, Resource, fields
 import jwt, uuid, os
-from functools import wraps
+from functools import wraps 
 from flask import abort, request, session
 from datetime import datetime
 
 
+# Preparing the Namespace of the search routes to add to main api file
 search = Namespace('/api/search', \
     description='This namespace contains all the search capabilities of the API, it involve the search for post, users \
         and other entities.', \
