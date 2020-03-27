d40edb2e05bd3bba5b038c358a714957c14649f2
Fabien Classic <touchone0001@gmail.com>
2020-03-27 04:27:20 +0100

    latestbot

--- AliceBot/AliceMini/AliceMini/spiders/webscraper.py
+++ AliceBot/AliceMini/AliceMini/spiders/webscraper.py
@@ -6,7 +6,7 @@ import json
 class WebscraperSpider(scrapy.Spider):
     name = 'webscraper'
     #allowed_domains = ['https://punchng.com'] good
-    urls=[ 'https://www.lindaikejisblog.com/', 'https://www.nairaland.com/', 'https://guardian.ng/category/news/nigeria/', 'https://www.channelstv.com/', 'https://www.premiumtimesng.com/', 'https://www.pmnewsnigeria.com/',  'https://www.independent.ng/',  'https://www.withinnigeria.com/']
+    urls=['https://www.scmp.com/rss/318421/']#'https://www.lindaikejisblog.com/', 'https://www.nairaland.com/', 'https://guardian.ng/category/news/nigeria/', 'https://www.channelstv.com/', 'https://www.premiumtimesng.com/', 'https://www.pmnewsnigeria.com/',  'https://www.independent.ng/',  'https://www.withinnigeria.com/']
     start_urls = []
     for url in urls: 
         final_url="https://api.rss2json.com/v1/api.json?rss_url=" + url +"feed"
@@ -17,8 +17,9 @@ class WebscraperSpider(scrapy.Spider):
     def parse(self, response):
         items = AliceminiItem()
         data = json.loads(response.text)
-        Title=data['feed']['title']
-        for i in range(4) :
+        Source=data['feed']['title']
+        Source_link=data['feed']['link']
+        for i in range(5) :
             Title_o_content=data['items'][i]['title']
             link=data['items'][i]['link']
             pubDate=data['items'][i]["pubDate"]
@@ -26,7 +27,5 @@ class WebscraperSpider(scrapy.Spider):
             content=data['items'][i]["content"]
             thumbnail=data['items'][i]["thumbnail"]
             categories=data['items'][i]['categories']
-        #Title=response.css('.seg-title::text').extract().....This part of code is for me to continue with the HTML Bot
-        #pubdate=response.css('.pull-right::text').extract()
-            print(Author)
+            print(link)
         #Sorry for the disorganised  code,i need to rearrange..it scrapes all the sites in the lists which are xml perfectly
--- AliceBot/AliceMini/AliceMini/spiders/webscraperHTML.py
+++ AliceBot/AliceMini/AliceMini/spiders/webscraperHTML.py
@@ -5,10 +5,13 @@ import scrapy
 class WebscraperhtmlSpider(scrapy.Spider):
     name = 'webscraperHTML'
     allowed_domains = ['amazon.com']
-    start_urls = ['https://www.informationng.com/','https://punchng.com/']
+    start_urls = ['https://www.informationng.com/']#,'https://punchng.com/'
 
     def parse(self, response):
-        Title=response.css(r'.+[a-z0-9\.\-+_]+-title')
-        date=response.css(r'.+[a-z0-9\.\-+_]+-date')
-        if date == []:
-            date=response.css('.pullright')
+        a='.td-module-title a::text'
+        Title=response.css(a).get()
+        #Title=response.css()
+        #date=response.css(r'.+[a-z0-9\.\-+_]+-date')
+       # if date == []:
+       #     date=response.css('.pullright')
+        print(Title)
