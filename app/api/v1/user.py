2e17994c66bca360af9d934da515da1f72f14823
The Polymath <das.sanctity.ds@gmail.com>
2020-04-06 17:01:55 +0100

    Tried to add continous integration

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -2,7 +2,7 @@ from flask_restplus import Namespace, Resource, fields
 import jwt, uuid, os
 from functools import wraps
 from flask import abort, request, session
-
+from app.models import Users
 
 # The token decorator to protect my routes
 def token_required(f):
@@ -27,32 +27,13 @@ user = Namespace('/api/user', \
     on the application.", \
     path = '/v1/')
 
-
-apiinfo = user.model('Info', {
+userinfo = user.model('Profile', {
     'name': fields.String,
     'version': fields.Integer,
     'date': fields.String,
     'author': fields.String,
     'description': fields.String
 })
-
-logindata = user.model('Login', {
-    'username': fields.String(required=True),
-    'password': fields.String(required=True)
-})
-
-verifydata = user.model('Verify', {
-    'username': fields.String(required=True),
-    'code': fields.String(required=True)
-})
-
-creationdata = user.model('Create', {
-    'username': fields.String(required=True),
-    'email': fields.String(required=True),
-    'password': fields.String(required=True),
-    'bio': fields.String(required=True)
-})
-    
 
 @user.doc(
     security='KEY',
@@ -69,24 +50,60 @@ creationdata = user.model('Create', {
         404: 'Resource Not found',
         500: 'internal server error, please contact admin and report issue'
     })
-@user.route('/user/<username>')
+@user.route('/user')
 class Data(Resource):
-    @user.marshal_with(apiinfo)
-    def get(self):
-        return {}, 200
     @token_required
-    @user.expect(logindata)
+    @user.marshal_with(userinfo)
+    def get(self):
+        username = request.args.get('username', None)
+        if username:
+            user =  Users.query.filter_by(username=username).first()
+            # get list of blocked users to make sure they don't see profile information.
+            if user is None:
+                return {'res': 'User not found'}, 404
+        else:
+            token = request.headers['API-KEY']
+            data = jwt.decode(token, app.config.get('SECRET_KEY'))
+            user = Users.query.filter_by(uuid=data['uuid']).first()
+        return user, 200
+    @token_required
+    @user.expect(userinfo)
     def post(self, username):
         return {}, 200
     @token_required
-    @user.expect(logindata)
+    @user.expect(userinfo)
     def put(self):
         return {}, 200
     @token_required
-    @user.expect(logindata)
+    @user.expect(userinfo)
     def patch(self):
         return {}, 200
     @token_required
-    @user.marshal_with(apiinfo)
+    @user.marshal_with(userinfo)
     def delete(self):
+        return {}, 200
+
+@user.doc(
+    security='KEY',
+    params={ 'username': 'Specify the username associated with the person' },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@user.route('/user/prefs')
+class Userprefs(Resource):
+    @user.marshal_with(userinfo)
+    def get(self):
+        return {}, 200
+    @token_required
+    @user.expect(userinfo)
+    def post(self, username):
         return {}, 200
