12de6fcfc70fc2973e85879638297fdec10e587e
Fabien Classic <touchone0001@gmail.com>
2020-09-11 12:00:31 +0100

    posts

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -150,14 +150,9 @@ Article_verify = post.model('postreq',{
     "Link":fields.String(required=True)
 })
 saved = post.model('saved',{
-    'id': fields.Integer(required=True),
-    'uuid':fields.String(required=True),
-    'title': fields.String(required=True),
-    'postchannel': fields.List(fields.Nested(channelfinal)),
-    'post_url': fields.String(required=True),
-    'uploader': fields.String(required=True),
-    'content': fields.String(required=True),
-    'uploader_date': fields.DateTime(required=True)
+    "content":fields.String(required=True),
+    "user_id":fields.String(required=True),
+    "post___id":fields.List(fields.Nested(user_post_sav)),
 })
 
 
