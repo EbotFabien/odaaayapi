1230c07fea07dcf16657b1181ef57b23e80a5d1e
The Polymath <das.sanctity.ds@gmail.com>
2020-10-28 13:14:27 +0100

    auth for postgres

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir= os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1234@localhost/news'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Ewenzo#@das67001@localhost/news'
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     # 'mysql://root:''@localhost/news' \
     #postgresql+psycopg2://postgres:1234@localhost/news
