fa53a8c8b9816be9ef23d915fd35f97565229117
fabienclassic <fabienclassic@gmail.com>
2021-07-07 18:33:41 +0100

    done

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -58,7 +58,7 @@ class MyApi(Api):
     @property
     def specs_url(self):
         """Monkey patch for HTTPS"""
-        scheme = 'http' if '5000' in self.base_url else 'https'
+        scheme = 'http' if '8000' in self.base_url else 'https'
         return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
 
 api = Blueprint('api', __name__, template_folder = '../templates')
