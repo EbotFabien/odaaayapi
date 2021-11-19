b900a1d1985a3f5fc494710840e459474497f292
fabienclassic <fabienclassic@gmail.com>
2021-11-19 15:36:44 +0100

    done

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -269,6 +269,7 @@ class Uplu(Resource):
             if Name.lower() =="png":
                 ex=user.username+".png"
             fil=os.path.join(destination,str(data['uuid']),ex)
+            os.mkdir(fil)
             with open(fil, 'wb') as image_file:
                 image_file.write(base64.b64encodebytes(File))
             user.picture=str(data['uuid'])+"/"+ex
