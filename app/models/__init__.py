a5ea3ea4a8a8b3488e5a598e2a8dde678420723a
Ebot Fabien <touchone0001@gmail.com>
2022-02-12 09:06:33 +0100

    done

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -453,7 +453,7 @@ class Posts(db.Model):
         db.session.commit()
         return task
 
-    def launch_summary_task(self, name, userid,app=app, descr):
+    def launch_summary_task(self, name, userid, descr):
         rq_job = app.task_queue.enqueue(
             'app.services.task.' + name, self.id,userid,app)
         task = Task(id=rq_job.get_id(), name=name,
