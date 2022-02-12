dd363612ae9b759ed3d270bc04132fda430a91b8
Ebot Fabien <touchone0001@gmail.com>
2022-02-12 09:14:32 +0100

    don

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -443,10 +443,10 @@ class Posts(db.Model):
         self.post_url = post_url
         self.video_url = video_url
 
-    def launch_translation_task(self, name, userid,app=app,descr):
+    def launch_translation_task(self, name, userid,descr):
         with app.app_context():
             rq_job = app.task_queue.enqueue(
-                'app.services.task.' + name, self.id,userid,app)
+                'app.services.task.' + name, self.id,userid)
         task = Task(id=rq_job.get_id(), name=name,
                     user_id=userid, description=descr)
         db.session.add(task)
@@ -455,7 +455,7 @@ class Posts(db.Model):
 
     def launch_summary_task(self, name, userid, descr):
         rq_job = app.task_queue.enqueue(
-            'app.services.task.' + name, self.id,userid,app)
+            'app.services.task.' + name, self.id,userid)
         task = Task(id=rq_job.get_id(), name=name,
                     user_id=userid, description=descr)
         db.session.add(task)
