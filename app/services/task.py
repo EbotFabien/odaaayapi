c2b6077fcf14bd3bee438ca5cb1d499168b75a40
Ebot Fabien <touchone0001@gmail.com>
2022-02-13 11:09:55 +0100

    done

--- app/services/task.py
+++ app/services/task.py
@@ -65,59 +65,60 @@ def export_posts(user_id):
 
 def translate_posts(post_id, user_id):
     languages = ['en', 'es', 'pt', 'sw', 'ha', 'ar', 'fr']
-    post = Posts.query.get(post_id)
-    category=Category.query.get(post.category_id)
-    if post:
-        user = Users.query.get(user_id)
-        post_auto_lang = translator.detect(post.title)
-    user_default_lang = str(post_auto_lang.lang)
-    post_language = Language.query.filter_by(code=user_default_lang).first()
-    sum_content = ''
-    # tag collector
-   # rake = Rake()
+    with app.app_context():
+        post = Posts.query.get(post_id)
+        category=Category.query.get(post.category_id)
+        if post:
+            user = Users.query.get(user_id)
+            post_auto_lang = translator.detect(post.title)
+        user_default_lang = str(post_auto_lang.lang)
+        post_language = Language.query.filter_by(code=user_default_lang).first()
+        sum_content = ''
+        # tag collector
+    # rake = Rake()
 
-    
-    parser = HtmlParser.from_string(post.text_content, '', Tokenizer(post_language.name))
-    stemmer = Stemmer(post_language.name)
-    summarizer = Summarizer(stemmer)
-    summarizer.stop_words = get_stop_words(post_language.name)
+        
+        parser = HtmlParser.from_string(post.text_content, '', Tokenizer(post_language.name))
+        stemmer = Stemmer(post_language.name)
+        summarizer = Summarizer(stemmer)
+        summarizer.stop_words = get_stop_words(post_language.name)
 
-    for sentence in summarizer(parser.document, 2):
-        sum_content += '\n'+str(sentence)
-    try:
-        for j in languages:
-            if j == user_default_lang:
-                current_lang = Language.query.filter_by(code=user_default_lang).first()
-                #table = language_dict.get(user_default_lang)
-                #keywords = rake.apply(sum_content)
-                if post is not None:
-                    new_check =Translated.query.filter(and_(Translated.title==post.title,Translated.language_id==current_lang.id)).first()
-                    if new_check is None:
-                        new_row = Translated(post_id=post_id,category=category.name,category_id=category.id,title=post.title,user=post.user_name,content=sum_content,language_id=current_lang.id,fullcontent=post.text_content, tags=post.tags)#[x[0] for x in keywords[:5]]))
-                        db.session.add(new_row)
-                        db.session.commit()
-        with app.app_context():
+        for sentence in summarizer(parser.document, 2):
+            sum_content += '\n'+str(sentence)
+        try:
+            for j in languages:
+                if j == user_default_lang:
+                    current_lang = Language.query.filter_by(code=user_default_lang).first()
+                    #table = language_dict.get(user_default_lang)
+                    #keywords = rake.apply(sum_content)
+                    if post is not None:
+                        new_check =Translated.query.filter(and_(Translated.title==post.title,Translated.language_id==current_lang.id)).first()
+                        if new_check is None:
+                            new_row = Translated(post_id=post_id,category=category.name,category_id=category.id,title=post.title,user=post.user_name,content=sum_content,language_id=current_lang.id,fullcontent=post.text_content, tags=post.tags)#[x[0] for x in keywords[:5]]))
+                            db.session.add(new_row)
+                            db.session.commit()
+            
             title_translation = app.ts.translate(text=post.title, src=user_default_lang, dest=languages)
             content_translation = app.ts.translate(text=sum_content, src=user_default_lang, dest=languages)
             full_content = app.ts.translate(text=post.text_content, src=user_default_lang, dest=languages)
-        p = 1
-        for i in tqdm(languages):
-            # _set_task_progress(p/len(languages) * 100)
-            for j in languages:
-                if i == j and i != user_default_lang:
-                   current_lang = Language.query.filter_by(code=i).first()
-                   # table = language_dict.get(i)
-                   #keywords = rake.apply(content_translation[i])
-                   new_check =Translated.query.filter(and_(Translated.title==title_translation[i],Translated.language_id==current_lang.id)).first()
-                   if new_check is None:
-                        new_row = Translated(post_id=post_id,category=category.name,category_id=category.id,fullcontent=full_content[i],user=post.user_name,title=title_translation[i],content=content_translation[i],language_id=current_lang.id, tags=post.tags)#[x[0] for x in keywords[:5]]))
-                        db.session.add(new_row)
-                        db.session.commit()
-                        p += 1         
-    except:
-        _set_task_progress(100)
-        with app.app_context():
-            app.logger.error('Unhandled exception', exc_info=sys.exc_info())
+            p = 1
+            for i in tqdm(languages):
+                # _set_task_progress(p/len(languages) * 100)
+                for j in languages:
+                    if i == j and i != user_default_lang:
+                    current_lang = Language.query.filter_by(code=i).first()
+                    # table = language_dict.get(i)
+                    #keywords = rake.apply(content_translation[i])
+                    new_check =Translated.query.filter(and_(Translated.title==title_translation[i],Translated.language_id==current_lang.id)).first()
+                    if new_check is None:
+                            new_row = Translated(post_id=post_id,category=category.name,category_id=category.id,fullcontent=full_content[i],user=post.user_name,title=title_translation[i],content=content_translation[i],language_id=current_lang.id, tags=post.tags)#[x[0] for x in keywords[:5]]))
+                            db.session.add(new_row)
+                            db.session.commit()
+                            p += 1         
+        except:
+            _set_task_progress(100)
+            with app.app_context():
+                app.logger.error('Unhandled exception', exc_info=sys.exc_info())
 
 
 def summarize_posts(post_id, user_id):
