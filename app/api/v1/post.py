19608cf0a0a1c65911efa013e52672f32a3b9ec0
fabienclassic <fabienclassic@gmail.com>
2022-02-19 12:04:10 +0100

    'done'

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -263,21 +263,23 @@ class ptag(Resource):
                 str(int(start)+1)+"&limit="+limit+"&count="+count
             previous = "/api/v1/post/tags?start=" + \
                 str(int(start)-1)+"&limit="+limit+"&count="+count
-            results1=[]
+            
             if category == None:
                 results = Tags.query.distinct(Tags.tags).paginate(    #order_by(func.random())
                     int(start), int(count), False).items
                 for tag in results:
+                    results1=[]
                     total=Tags.query.filter_by(tags=tag.tags).count()
-                    #if total > 5:
-                    results1.append(tag)
+                    if total > 5:
+                        results1.append(tag)
             else:  
                 results = Tags.query.distinct(Tags.tags).join(Posts, (Posts.id == Tags.post)).filter(   #order_by(func.random())
                     Posts.category_id == category).paginate(int(start), int(count), False).items
                 for tag in results:
+                    results1=[]
                     total=Tags.query.filter_by(tags=tag.tags).count()
-                    #if total > 5:
-                    results1.append(tag)
+                    if total > 5:
+                        results1.append(tag)
             return {
                 "start": start,
                 "limit": limit,
