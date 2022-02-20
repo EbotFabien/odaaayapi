0c785ff1ffc24c2477d83d84253536c7ad2ddbdb
fabienclassic <fabienclassic@gmail.com>
2022-02-20 01:52:13 +0100

    'done'

--- app/api/v1/post.py
+++ app/api/v1/post.py
@@ -270,23 +270,23 @@ class ptag(Resource):
                 for tag in results:
                     results1=[]
                     total=Tags.query.filter_by(tags=tag.tags).count()
-                    if total < 5:
-                        results.remove(tag)
+                    if total >= 5:
+                        results1.append(tag)
             else:  
                 results = Tags.query.distinct(Tags.tags).join(Posts, (Posts.id == Tags.post)).filter(   #order_by(func.random())
                     Posts.category_id == category).paginate(int(start), int(count), False).items
                 for tag in results:
                     results1=[]
                     total=Tags.query.filter_by(tags=tag.tags).count()
-                    if total < 5:
-                        results.remove(tag)
+                    if total >= 5:
+                        results1.append(tag)
             return {
                 "start": start,
                 "limit": limit,
                 "count": count,
                 "next": next,
                 "previous": previous,
-                "results": marshal(results, tegs)
+                "results": marshal(results1, tegs)
             }, 200
 
 
