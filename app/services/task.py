99584b3c315576dda0558954f950da0bcf7c7d05
Ebot Fabien <touchone0001@gmail.com>
2023-05-09 11:48:00 +0100

    done

--- app/services/task.py
+++ app/services/task.py
@@ -141,7 +141,7 @@ def export_posts(user_id):
         app.logger.error('Unhandled exception', exc_info=sys.exc_info())
 
 def translate_posts(post_id, user_id):
-    languages = ['en', 'es','pt', 'sw', 'ha', 'ar', 'fr']
+    languages = ['en', 'es','pt', 'sw', 'ar', 'fr'] #, 'ha'
     post = Posts.query.get(post_id)
     category=Category.query.get(post.category_id)
     if post:
