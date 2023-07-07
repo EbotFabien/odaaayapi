004cfd634b4da0c4bcf89b5624cd0072014ac4b1
Ebot <touchone0001@gmail.com>
2023-07-07 20:16:38 +0200

    done

--- news.py
+++ news.py
@@ -94,7 +94,7 @@ def category():
 
 @manager.command
 def run():
-    #logo()
+    logo()
     # Error tracking and logging with sentry
     sentry_sdk.init(
         dsn="https://076148b85ca74c93b2c9ab0e07c2bd24@o1249285.ingest.sentry.io/6409744",
