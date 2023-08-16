2c4105362401cf20cd95b276923094be213c2bb4
Ebot <touchone0001@gmail.com>
2023-08-16 21:05:40 +0200

    'done'

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ class Config(object):
     PAGINATE_PAGE_PARAM = "pagenumber"
     PAGINATE_SIZE_PARAM = "pagesize"   
     PAGINATE_RESOURCE_LINKS_ENABLED = True
-    REDIS_URL ='redis://adminnews@localhost:6379'#os.environ.get('REDIS_URL') or 'redis://'
+    REDIS_URL ='redis://:adminnews@localhost:6379'#os.environ.get('REDIS_URL') or 'redis://'
     RQ_DASHBOARD_USERNAME='rqadmin'
     RQ_DASHBOARD_PASSWORD='adminnews'
     QUEUES = ['default']
