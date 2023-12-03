03627e351677c492d829586b6bbb99d39253be71
Ebot <touchone0001@gmail.com>
2023-12-03 09:56:37 +0100

    'done'

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -354,9 +354,9 @@ class Reset(Resource):
                 verification_code = phone.generate_code()
                 token = check.get_reset_token()
                 if lang=='en':
-                    link='https://odaaay.com/app/reset-password/?token='+token
+                    link='https://odaaay.com/reset-password/?token='+token
                 else:
-                    link='https://odaaay.com/'+lang+'/app/reset-password/?token='+token
+                    link='https://odaaay.com/'+lang+'/reset-password/?token='+token
                 
                 mail.reset_password(email, link)
                 return {
